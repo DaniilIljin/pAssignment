2985cd802d48fef70b0da6278a65096f4124f3c0
Daniil <dailji@ttu.ee>
2023-11-19 20:57:05 +0200

    added test: result contains illegal bet action

--- src/test/java/edgeCases/BetProcessorTests.java
+++ src/test/java/edgeCases/BetProcessorTests.java
@@ -5,6 +5,7 @@ import main.BetProcessor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BetProcessorTests {
 
@@ -25,5 +26,23 @@ public class BetProcessorTests {
 
         // Assert
         assertEquals(expectedContent, actualContent);
+    }
+
+    @Test
+    public void givenInputFiles_whenRunMethodCalled_thenResultFileContainsIllegalBet() {
+        // Arrange
+        BetProcessor betProcessor = new BetProcessor(
+                "src/test/resources/match_data.txt",
+                "src/test/resources/illegal_bet_player_data.txt",
+                "src/test/java/edgeCases/result.txt");
+
+        String expectedIllegalBetLine = "163f23ed-e9a9-4e54-a5b1-4e1fc86f12f4 BET abae2255-4255-4304-8589-737cdff61640 500 A";
+
+        // Act
+        betProcessor.run();
+        String actualContent = String.join("", Helper.readFromFile("src/test/java/edgeCases/result.txt"));
+
+        // Assert
+        assertTrue(actualContent.contains(expectedIllegalBetLine));
     }
 }
